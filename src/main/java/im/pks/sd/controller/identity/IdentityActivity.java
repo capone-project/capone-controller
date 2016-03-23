9d4f0b489fae5f352c6b2332c758e9aa65b8c4b7
Patrick Steinhardt <ps@pks.im>
2016-03-23 14:19:08 +0100

    controller: implement key import

--- src/main/java/im/pks/sd/controller/identity/IdentityActivity.java
+++ src/main/java/im/pks/sd/controller/identity/IdentityActivity.java
@@ -17,12 +17,18 @@
 
 package im.pks.sd.controller.identity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import im.pks.sd.controller.R;
 import im.pks.sd.persistence.Identity;
+import org.abstractj.kalium.encoders.Encoder;
 import org.abstractj.kalium.keys.SigningKey;
 
 import java.util.Iterator;
@@ -50,11 +56,43 @@ public class IdentityActivity extends AppCompatActivity {
             identity = identities.next();
         }
 
-        publicKey.setText(identity.getKey().getVerifyKey().toString());
-        keySeed.setText(identity.getKey().toString());
+        setKey(identity.getKey().toString());
     }
 
     public void onImportClicked(View view) {
+        final EditText seedInput = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.paste_your_key_seed);
+        builder.setView(seedInput);
+        builder.setPositiveButton(R.string.import_key, null);
+        builder.setNegativeButton(R.string.cancel, null);
+
+        final AlertDialog alertDialog = builder.create();
+
+        // Override default onClickListener to avoid dismissing dialog
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String input = seedInput.getText().toString();
+
+                        try {
+                            setKey(input);
+                            alertDialog.dismiss();
+                        } catch (Exception e) {
+                            Toast.makeText(IdentityActivity.this, R.string.invalid_seed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        alertDialog.show();
     }
 
     public void onGenerateClicked(View view) {
@@ -64,6 +102,16 @@ public class IdentityActivity extends AppCompatActivity {
 
         identity.setKey(signingKey);
         identity.save();
+    }
+
+    private void setKey(String input) {
+        byte[] seed = Encoder.HEX.decode(input);
+        SigningKey key = new SigningKey(seed);
+        identity.setKey(key);
+        identity.save();
+
+        publicKey.setText(identity.getKey().getVerifyKey().toString());
+        keySeed.setText(identity.getKey().toString());
     }
 
 }
