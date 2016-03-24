910099f4dca17b1d80ae2d3af3cbd6a893919e5f
Patrick Steinhardt <ps@pks.im>
2016-03-24 13:07:08 +0100

    controller: extract service list adapter class

--- src/main/java/im/pks/sd/controller/discovery/ServerDetailActivity.java
+++ src/main/java/im/pks/sd/controller/discovery/ServerDetailActivity.java
@@ -20,10 +20,6 @@ package im.pks.sd.controller.discovery;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import im.pks.sd.controller.R;
@@ -47,40 +43,19 @@ public class ServerDetailActivity extends Activity {
         TextView addressView = (TextView) findViewById(R.id.server_address);
         addressView.setText(server.address);
 
-        final ListView serviceList = (ListView) findViewById(R.id.service_list);
-        serviceList.setAdapter(new ArrayAdapter<Service>(this, R.layout.list_item_server, server.services) {
+        ListView serviceList = (ListView) findViewById(R.id.service_list);
+        ServiceListAdapter adapter = new ServiceListAdapter(this) {
             @Override
-            public View getView(final int position, View view, ViewGroup group) {
-                if (view == null) {
-                    view = View.inflate(ServerDetailActivity.this, R.layout.list_item_service, null);
-                }
-
-                final Service service = server.services.get(position);
-
-                view.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(ServerDetailActivity.this,
-                                ServiceDetailActivity.class);
-                        intent.putExtra(ServiceDetailActivity.EXTRA_SERVICE, service);
-                        intent.putExtra(ServiceDetailActivity.EXTRA_SERVER, server);
-                        startActivity(intent);
-                    }
-                });
-
-                ImageView serviceImage = (ImageView) view.findViewById(R.id.service_image);
-                serviceImage.setImageResource(service.getResourceId());
-
-                TextView serviceName = (TextView) view.findViewById(R.id.service_name);
-                serviceName.setText(service.name);
-                TextView serviceType = (TextView) view.findViewById(R.id.service_type);
-                serviceType.setText(service.type);
-                TextView servicePort = (TextView) view.findViewById(R.id.service_port);
-                servicePort.setText(String.valueOf(service.port));
-
-                return view;
+            public void onServiceClicked(Service service) {
+                Intent intent = new Intent(ServerDetailActivity.this,
+                                                  ServiceDetailActivity.class);
+                intent.putExtra(ServiceDetailActivity.EXTRA_SERVICE, service);
+                intent.putExtra(ServiceDetailActivity.EXTRA_SERVER, server);
+                startActivity(intent);
             }
-        });
+        };
+        adapter.addAll(server.services);
+        serviceList.setAdapter(adapter);
     }
 
 }
---
+++ src/main/java/im/pks/sd/controller/discovery/ServiceListAdapter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 Patrick Steinhardt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package im.pks.sd.controller.discovery;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import im.pks.sd.controller.R;
+
+public abstract class ServiceListAdapter extends ArrayAdapter<Service> {
+
+    public ServiceListAdapter(Context context) {
+        super(context, R.layout.list_item_service);
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup group) {
+        final Service service = getItem(position);
+
+        if (view == null) {
+            view = View.inflate(getContext(), R.layout.list_item_service, null);
+        }
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onServiceClicked(service);
+            }
+        });
+
+        ImageView serviceImage = (ImageView) view.findViewById(R.id.service_image);
+        serviceImage.setImageResource(service.getResourceId());
+
+        TextView serviceName = (TextView) view.findViewById(R.id.service_name);
+        serviceName.setText(service.name);
+        TextView serviceType = (TextView) view.findViewById(R.id.service_type);
+        serviceType.setText(service.type);
+        TextView servicePort = (TextView) view.findViewById(R.id.service_port);
+        servicePort.setText(String.valueOf(service.port));
+
+        return view;
+    }
+
+    public abstract void onServiceClicked(Service service);
+
+}
