c26d0bcc823b66112aaa32f4c24356bdcc1e829c
Patrick Steinhardt <ps@pks.im>
2016-04-06 10:30:24 +0200

    controller: mitigate inflater warning on null view group

--- src/main/java/im/pks/sd/controller/invoke/ServiceChooserDialog.java
+++ src/main/java/im/pks/sd/controller/invoke/ServiceChooserDialog.java
@@ -21,7 +21,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -44,7 +43,7 @@ public abstract class ServiceChooserDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_service_chooser, null);
+        View view = View.inflate(getActivity(), R.layout.dialog_service_chooser, null);
         list = (ListView) view.findViewById(R.id.server_list);
 
         Button cancel = (Button) view.findViewById(R.id.button_cancel);
